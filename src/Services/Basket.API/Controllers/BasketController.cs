8c7bf1b394792e66bd586deeee22f8cc013d4df7
M. A. R. BIN SIDDIQUI MAMUN <mamunslogic@gmail.com>
2024-05-17 22:50:24 +0600

    Basket.API implemented

---
+++ src/Services/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,69 @@
+﻿using Basket.API.Models;
+using Basket.API.Repositories;
+using CoreApiResponse;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class BasketController : BaseController
+    {
+        IBasketRepository _basketRepository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetBasket(string username)
+        {
+            try
+            {
+                var basket = await _basketRepository.GetBasket(username);
+                if (basket is null)
+                {
+                    return CustomResult("Load successful.", HttpStatusCode.NotFound);
+                }
+                return CustomResult("Load successful.", basket, HttpStatusCode.OK);
+            }
+            catch (Exception exception)
+            {
+                return CustomResult(exception.Message, HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            try
+            {
+                var updatedBasket = await _basketRepository.UpdateBasket(basket);
+                return CustomResult("Update successful.", updatedBasket, HttpStatusCode.OK);
+            }
+            catch (Exception exception)
+            {
+                return CustomResult(exception.Message, HttpStatusCode.BadRequest);
+            }
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string username)
+        {
+            try
+            {
+                await _basketRepository.DeleteBasket(username);
+                return CustomResult("Delete successful.", HttpStatusCode.OK);
+            }
+            catch (Exception exception)
+            {
+                return CustomResult(exception.Message, HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
---
+++ src/Services/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,30 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart(string username)
+        {
+            Username = username;
+        }
+
+        public ShoppingCart()
+        {
+
+        }
+
+        public string Username { get; set; }
+        public List<ShoopingCartItem> Items { get; set; } = [];
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in Items)
+                {
+                    totalPrice += item.Price;
+                }
+                return totalPrice;
+            }
+        }
+    }
+}
