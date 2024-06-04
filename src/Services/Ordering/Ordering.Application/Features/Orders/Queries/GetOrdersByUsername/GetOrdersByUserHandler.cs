5e2a2fb23d8634854ff30f0e87a1a55da5b65612
Mamun <mamunslogic@hotmail.com>
2024-06-04 12:17:44 +0600

    Add Ordering folder to implement clean architecture. Following projects are added under the folder. Ordering.API Ordering.Application Ordering.Domain Ordering.Infrastructure

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersByUsername/GetOrdersByUserHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersByUsername
+{
+    public class GetOrdersByUserHandler : IRequestHandler<GetOrdersByUserQuery, List<OrderViewModel>>
+    {
+        IOrderRepository _orderRepository;
+        IMapper _mapper;
+
+        public GetOrdersByUserHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<OrderViewModel>> Handle(GetOrdersByUserQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _orderRepository.GetOrdersByUsername(request.Username);
+            return _mapper.Map<List<OrderViewModel>>(orders);
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersByUsername/GetOrdersByUserQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersByUsername
+{
+    public class GetOrdersByUserQuery : IRequest<List<OrderViewModel>>
+    {
+        public string Username { get; set; }
+
+        public GetOrdersByUserQuery(string username)
+        {
+            Username = username;
+        }
+    }
+}
