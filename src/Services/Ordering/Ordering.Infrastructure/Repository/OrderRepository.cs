0fe1319b4beab1901c363539465abd95e3133b05
M. A. R. BIN SIDDIQUI MAMUN <mamunslogic@gmail.com>
2024-06-16 00:49:33 +0600

    Order application is tested and working fine.

--- src/Services/Ordering/Ordering.Infrastructure/Repository/OrderRepository.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Repository/OrderRepository.cs
@@ -17,7 +17,7 @@ namespace Ordering.Infrastructure.Repository
 
         public async Task<IEnumerable<Order>> GetOrdersByUsername(string username)
         {
-            var orderList = await _dbContext.Orders.Where(s => s.Username.Equals(username, StringComparison.CurrentCultureIgnoreCase)).ToListAsync();
+            var orderList = await _dbContext.Orders.Where(s => s.Username.ToUpper() == username.ToUpper()).ToListAsync();
             return orderList;
         }
     }
